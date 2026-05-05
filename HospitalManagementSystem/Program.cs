c1c0c5ea7dabb6dd8d8c60c622bbd1721c1d01dc
Jamie <jamiegrant2002@hotmail.com>
2026-05-05 15:40:57 +0100

    Added packages

--- HospitalManagementSystem/Program.cs
+++ HospitalManagementSystem/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.Signin.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
