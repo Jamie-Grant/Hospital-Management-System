939472db3271264351456cb48f406d2e973edc18
Jamie <jamiegrant2002@hotmail.com>
2026-05-05 15:51:45 +0100

    Scaffolded Identity Pages

--- HospitalManagementSystem/Program.cs
+++ HospitalManagementSystem/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDefaultIdentity<HospitalManagementSystemUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<HospitalManagementSystemContext>();
+
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
