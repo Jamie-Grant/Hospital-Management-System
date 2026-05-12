b49d98693b3a17ff86abc1a28ac65e9be28734d5
Jamie <jamiegrant2002@hotmail.com>
2026-05-12 15:34:44 +0100

    Added user registration

--- HospitalManagementSystem/Controllers/AccountController.cs
+++ HospitalManagementSystem/Controllers/AccountController.cs
@@ -1,18 +1,54 @@
 ﻿using HospitalManagementSystem.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HospitalManagementSystem.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        
+}
         public IActionResult Register()
         {
             return View();
         }
 
+        [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            
+            if (ModelState.IsValid) 
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Username,
+                    Email = model.Email,
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return RedirectToAction("index", "Home");
+                }
+
+                foreach (var error in result.Errors) 
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                ModelState.AddModelError(string.Empty, "Invalid");
+            }
+
+            return View(model);
         }
     }
 }
